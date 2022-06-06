8e5f5e52906d7f96d74fa6124d587c9002377b0a
Jannes Behrens <jannes@MacBook-Pro-von-Jannes.fritz.box>
2022-06-06 22:04:48 +0200

    fix dep

--- publishes/resources/pages/components/PanelContentSidebar.vue
+++ publishes/resources/pages/components/PanelContentSidebar.vue
@@ -5,13 +5,14 @@
 </template>
 
 <script lang="ts" setup>
+import { ContentSidebar } from '@macramejs/admin-vue3';
 import { Drawers, sections } from '@admin/modules/content';
 import { ref } from 'vue';
 
 // allow all sections except for `block`
 let drawsSections = {};
-for(let key in sections) {
-    if(key != 'blocks') drawsSections[key] = true;
+for (let key in sections) {
+    if (key != 'blocks') drawsSections[key] = true;
 }
 
 const isOpen = ref(true);
